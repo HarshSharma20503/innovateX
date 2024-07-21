a97dcc9be6e76b9a1f4ed4fb83c44568d6b0af21
sparshkumar007 <sparshrajput92@gmail.com>
2024-07-21 07:27:18 +0530

    errors resolved 4

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -348,7 +348,7 @@ export const orderDetails = AsyncHandler(async (req, res) => {
 
   console.log("track: ", trackRecord);
   // order_user_status
-  const order_user_track = trackRecord.find((x) => x.id === user._id);
+  const order_user_track = trackRecord.find((x) => x.id == user._id);
   order_user_status = {
     sent: order_user_track?.send_status ? order_user_track.send_status : false,
     recieved: order_user_track?.recieve_status ? order_user_track.recieve_status : false,
