abccc631ea59d14ed3e7a18267050bf4c732f417
Harsh Sharma <harshsharma20503@gmail.com>
2024-07-21 07:45:45 +0530

    integrated order details confirmed

--- frontend/src/pages/ItemDetails/ItemDetails.jsx
+++ frontend/src/pages/ItemDetails/ItemDetails.jsx
@@ -12,6 +12,9 @@ import { GetApiCall } from "../../utils/Axios";
 
 const ItemDetails = () => {
   const { id } = useParams();
+
+  const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem("userInfo")));
+
   const [showModal, setShowModal] = useState(false);
   const [showQTPModal, setShowOTPModal] = useState(false);
   const [showSendOTPModal, setShowSendOTPModal] = useState(false);
@@ -54,6 +57,7 @@ const ItemDetails = () => {
   useEffect(() => {
     // Fetch item details from backend
     const getItemDetails = async () => {
+      setLoading(true);
       const response = await GetApiCall(`api/order/orderDetails/${id}`);
       console.log(response);
       if (response.success) {
@@ -61,6 +65,7 @@ const ItemDetails = () => {
         setOrderStatus(response.data.order_user_status);
         setTrackRecord(response.data.trackRecord);
       }
+      setLoading(false);
     };
     getItemDetails();
   }, []);
@@ -87,129 +92,152 @@ const ItemDetails = () => {
         {/* Items Details */}
         <Card className="mb-4" style={{ borderRadius: "15px", backgroundColor: "#f8f9fa" }}>
           <Card.Header as="h5">Item Details</Card.Header>
-          <Card.Body>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Item</th>
-                  <th>Details</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Iteam Name</td>
-                  <td>{itemDetails.itemName}</td>
-                </tr>
-                <tr>
-                  <td>From</td>
-                  <td>{itemDetails.from}</td>
-                </tr>
-                <tr>
-                  <td>To</td>
-                  <td>{itemDetails.to}</td>
-                </tr>
-                <tr>
-                  <td>Image</td>
-                  <td>
-                    <img src={itemDetails.imgUrl} />
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </Card.Body>
+          {loading ? (
+            <div className="w-100 my-4 text-center">
+              <Spinner className="" animation="border" variant="primary" />
+            </div>
+          ) : (
+            <Card.Body>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Item</th>
+                    <th>Details</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Iteam Name</td>
+                    <td>{itemDetails.itemName}</td>
+                  </tr>
+                  <tr>
+                    <td>From</td>
+                    <td>{itemDetails.from}</td>
+                  </tr>
+                  <tr>
+                    <td>To</td>
+                    <td>{itemDetails.to}</td>
+                  </tr>
+                  <tr>
+                    <td>Image</td>
+                    <td>
+                      <img src={itemDetails.imgUrl} className="img-fluid" />
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Card.Body>
+          )}
         </Card>
 
         {/* Actions */}
         <Card className="mb-4 p-0" style={{ borderRadius: "15px", backgroundColor: "#f8f9fa" }}>
           <Card.Header as="h5">Actions you need to take</Card.Header>
-          <Card.Body>
-            <div className="container d-flex justify-content-around m-0">
-              <Card className="text-center w-100 mx-0">
-                <Card.Header as="h6">Recieve Item from</Card.Header>
-                <Table striped bordered hover responsive="sm">
-                  <tbody>
-                    <tr>
-                      <td>From</td>
-                      <td>{itemDetails.from}</td>
-                    </tr>
-                    <tr>
-                      <td>To</td>
-                      <td>{itemDetails.to}</td>
-                    </tr>
-                    <tr>
-                      <td>Confirm Your Identity</td>
-                      <td>
-                        <Button variant="primary" onClick={handleShowOTPModal}>
-                          Get Otp
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Validate Item</td>
-                      <td>
-                        <Button variant="primary" onClick={handleShowModal}>
-                          Scan
-                        </Button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </Card>
-              <Card className="text-center w-100 me-0">
-                <Card.Header as="h6">Give Item To</Card.Header>
-
-                <Table striped bordered hover responsive="sm">
-                  <tbody>
-                    <tr>
-                      <td>From</td>
-                      <td>{itemDetails.from}</td>
-                    </tr>
-                    <tr>
-                      <td>To</td>
-                      <td>{itemDetails.to}</td>
-                    </tr>
-                    <tr>
-                      <td>Confirm Reciever Identity</td>
-                      <td>
-                        <Button variant="primary" onClick={handleShowSendOTPModal}>
-                          Send Otp
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Delivery Status</td>
-                      <td>pending</td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </Card>
+          {loading ? (
+            <div className="w-100 my-4 text-center">
+              <Spinner className="" animation="border" variant="primary" />
             </div>
-          </Card.Body>
+          ) : (
+            <Card.Body>
+              <div className="container d-flex justify-content-around m-0">
+                {(userInfo.userType == "middle" || userInfo.userType == "Buyer") && (
+                  <Card className="text-center w-100 mx-0">
+                    <Card.Header as="h6">Recieve Item from</Card.Header>
+                    <Table striped bordered hover responsive="sm">
+                      <tbody>
+                        <tr>
+                          <td>From</td>
+                          <td>{itemDetails.from}</td>
+                        </tr>
+                        <tr>
+                          <td>To</td>
+                          <td>{itemDetails.to}</td>
+                        </tr>
+                        <tr>
+                          <td>Confirm Your Identity</td>
+                          <td>
+                            <Button variant="primary" onClick={handleShowOTPModal}>
+                              Get Otp
+                            </Button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Validate Item</td>
+                          <td>
+                            <Button variant="primary" onClick={handleShowModal}>
+                              Scan
+                            </Button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                  </Card>
+                )}
+                {(userInfo.userType == "middle" || userInfo.userType == "Seller") && (
+                  <Card className="text-center w-100 me-0">
+                    <Card.Header as="h6">Give Item To</Card.Header>
+
+                    <Table striped bordered hover responsive="md">
+                      <tbody>
+                        <tr>
+                          <td>From</td>
+                          <td>{itemDetails.from}</td>
+                        </tr>
+                        <tr>
+                          <td>To</td>
+                          <td>{itemDetails.to}</td>
+                        </tr>
+                        <tr>
+                          <td>Confirm Reciever Identity</td>
+                          <td>
+                            <Button variant="primary" onClick={handleShowSendOTPModal}>
+                              Send Otp
+                            </Button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Delivery Status</td>
+                          <td>pending</td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                  </Card>
+                )}
+              </div>
+            </Card.Body>
+          )}
         </Card>
 
         {/* Track Order */}
-        <Card className="mb-4" style={{ borderRadius: "15px", backgroundColor: "#f8f9fa" }}>
-          <Card.Header as="h5">Track Order</Card.Header>
-          <Card.Body className="m-0">
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Persons</th>
-                </tr>
-              </thead>
-              <tbody>
-                {trackRecord.map((record, index) => (
-                  <tr key={record.id}>
-                    <td>{record.itemName}</td>
-                  </tr>
-                ))}
-                <tr>
-                  <td>Attribute 1</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Card.Body>
-        </Card>
+        {userInfo.userType != "middle" && (
+          <Card className="mb-4" style={{ borderRadius: "15px", backgroundColor: "#f8f9fa" }}>
+            <Card.Header as="h5">Track Order</Card.Header>
+            {loading ? (
+              <div className="w-100 my-4 text-center">
+                <Spinner className="" animation="border" variant="primary" />
+              </div>
+            ) : (
+              <Card.Body className="m-0">
+                <Table striped bordered hover responsive="sm">
+                  <thead>
+                    <tr>
+                      <th>Persons</th>
+                      <th>Email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {trackRecord.map((record, index) => (
+                      <tr key={record.id}>
+                        <td>{record.name}</td>
+                        <td>{record.email}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Card.Body>
+            )}
+          </Card>
+        )}
       </div>
     </>
   );
